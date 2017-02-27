6b5378fa75cf3ac46825da8a768cbe92ec47ea82
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-02-27 15:05:00 +0100

    feat(test): add api test

--- Tests/ManageControllerTest.php
+++ Tests/ManageControllerTest.php
@@ -28,4 +28,28 @@ class ManageControllerTest extends WebTestCase
         $response = json_decode($client->getResponse()->getContent(), true);
         $this->assertEquals([], $response);
     }
+
+    public function testAddAPI() {
+        $client = static::createClient();
+        $server = [
+            'HTTP_X-KBC-ManageApiToken' => MANAGE_API_TOKEN,
+            'HTTP_X-StorageApi-Token' => STORAGE_API_TOKEN,
+            'CONTENT_TYPE' => 'application/json'
+        ];
+        $body = '{
+              "component_id": "ex-generic-v2",
+              "friendly_name": "Testing keboola.oauth-v2",
+              "app_key": "123456",
+              "app_secret": "654321",
+              "auth_url": "https://oauth.example.com",
+              "token_url": "https://oauth.example.com/token",
+              "oauth_version": "2.0"
+            }';
+
+        $client->request('POST', '/oauth-v2/manage', [], [], $server, $body);
+        $response = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('created', $response['status']);
+    }
+
+
 }
