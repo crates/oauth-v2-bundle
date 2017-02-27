da181394a1ed39592fd7c287fee206355fce51bc
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-02-27 15:36:41 +0100

    fix(tests): validate records in DB

--- Tests/ManageControllerTest.php
+++ Tests/ManageControllerTest.php
@@ -2,17 +2,23 @@
 
 namespace Keboola\OAuthV2Bundle\Tests;
 
+use Doctrine\DBAL\Driver\Connection;
 use Keboola\Syrup\Test\WebTestCase;
 use Keboola\Temp\Temp;
 
 class ManageControllerTest extends WebTestCase
 {
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
     public function setUp() {
         self::bootKernel([
             'debug' => true
         ]);
-        $connection = static::$kernel->getContainer()->get('doctrine')->getConnection('oauth_providers');
-        $connection->exec(
+        $this->connection = static::$kernel->getContainer()->get('doctrine')->getConnection('oauth_providers');
+        $this->connection->exec(
             "TRUNCATE `consumers`"
         );
     }
@@ -49,6 +55,16 @@ class ManageControllerTest extends WebTestCase
         $client->request('POST', '/oauth-v2/manage', [], [], $server, $body);
         $response = json_decode($client->getResponse()->getContent(), true);
         $this->assertEquals('created', $response['status']);
+
+        $dbRecord = $this->connection->fetchAll('SELECT * FROM `consumers`')[0];
+        $this->assertEquals('ex-generic-v2', $dbRecord['component_id']);
+        $this->assertEquals('https://oauth.example.com', $dbRecord['auth_url']);
+        $this->assertEquals('https://oauth.example.com/token', $dbRecord['token_url']);
+        $this->assertEquals('Testing keboola.oauth-v2', $dbRecord['friendly_name']);
+        $this->assertEquals('', $dbRecord['request_token_url']);
+        $this->assertEquals('2.0', $dbRecord['oauth_version']);
+        $this->assertArrayHasKey('app_secret', $dbRecord);
+        $this->assertArrayHasKey('app_secret_docker', $dbRecord);
     }
 
 
